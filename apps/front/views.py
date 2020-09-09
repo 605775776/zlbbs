93de441d5dd938346ce9cfbde661addb0d66e15c
605775776 <605775776@qq.com>
2020-09-10 00:17:03 +0800

    0910

--- apps/front/views.py
+++ apps/front/views.py
@@ -2,10 +2,20 @@
 # 2020/8/16 13:05
 # author:dsw
 
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    views,
+    render_template)
 
 bp = Blueprint('front', __name__)
 
 @bp.route('/')
 def index():
     return 'front index'
+
+
+class SignupView(views.MethodView):
+    def get(self):
+        return render_template('front/front_signup.html')
+
+bp.add_url_rule('/signup/', view_func=SignupView.as_view('signup'))
