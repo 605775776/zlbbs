660f7860f30c566c39dd216806334c8908b27d0d
605775776@qq.com <605775776@qq.com>
2020-09-10 18:36:39 +0800

    0910 1836

--- apps/front/views.py
+++ apps/front/views.py
@@ -18,4 +18,7 @@ class SignupView(views.MethodView):
     def get(self):
         return render_template('front/front_signup.html')
 
+    def post(self):
+        pass
+
 bp.add_url_rule('/signup/', view_func=SignupView.as_view('signup'))
